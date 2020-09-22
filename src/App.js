15bee819999a85d2c0c851b275547fde98d15b92
bbrusniak <brusniak@cs.washington.edu>
2020-09-21 19:16:39 -0700

    Deleted default logo

--- src/App.js
+++ src/App.js
@@ -15,7 +15,6 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1>{this.state.text}</h1>
         </header>
         <AmplifySignOut />
