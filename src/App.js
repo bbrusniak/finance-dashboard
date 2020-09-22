e5a08f5bec8ebc1b432bd4b91367a6cc2203a9f1
bbrusniak <brusniak@cs.washington.edu>
2020-09-21 23:10:37 -0700

    Added user's name

--- src/App.js
+++ src/App.js
@@ -2,13 +2,27 @@ import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
+import { Auth } from 'aws-amplify';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      text: "finance dashboard"
+      text: "finance dashboard",
+      username: "",
     }
+  }
+
+  async componentDidMount() {
+    this.getUsername();
+  }
+
+  getUsername = async () => {
+    const tokens = await Auth.currentSession();
+    const userName = tokens.getIdToken().payload['cognito:username'];
+    this.setState({
+      username: userName,
+    })
   }
 
   render() {
@@ -16,6 +30,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <h1>{this.state.text}</h1>
+          <p>Hello {this.state.username}</p>
         </header>
         <AmplifySignOut />
       </div>
